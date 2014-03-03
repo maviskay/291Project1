d3131c2583475bcc52877fef856fbde6704563c7
Mavis Chan <maviskay@ualberta.ca>
2014-03-03 02:36:41 -0700

    Added base file for project, has not been compiled & tested

---
+++ database.java
@@ -0,0 +1,36 @@
+//import java.io.*;
+//import java.sql.*;
+
+public class database {
+
+	public static void main(String[] argv) {
+		/*String driver = "oracle.jdbc.driver.OracleDriver";
+		String database ="dbc:oracle:thin:@gwynne.cs.ualberta.ca:1521:CRS";
+		String username;
+		String password;
+		Connection conn = null;*/
+
+		System.out.println("CMPUT 291 Project 1");
+
+		//if (argv.length != 2)
+		//	System.out.println("Enter oracle username followed by password");
+
+		/*try {
+			Class.forName(driver);
+		} catch (ClassNotFoundException e) {
+			System.out.println("Oracle JDBC Driver not found");
+			return;
+		}
+
+		try{
+			conn = DriverManager.getConnected(database, username, password);
+		} catch (SQLException e) {
+			System.out.println("Connection to Oracle failed");
+			return;
+ 
+		}
+		
+		if (conn != null)
+			System.out.println("Connection established");*/
+	}
+}
