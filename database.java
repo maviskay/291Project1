d130e19ec6c020fafb26e1608fae589ac64fcff3
Mavis Chan <maviskay@ualberta.ca>
2014-03-05 00:56:20 -0700

    Added arguments for username and password

--- database.java
+++ database.java
@@ -1,36 +1,37 @@
-import java.io.*;
 import java.sql.*;
 
 public class database {
 
 	public static void main(String[] argv) {
-		/*String driver = "oracle.jdbc.driver.OracleDriver";
-		String database ="dbc:oracle:thin:@gwynne.cs.ualberta.ca:1521:CRS";
+		String driver = "oracle.jdbc.driver.OracleDriver";
+		String database ="jdbc:oracle:thin:@gwynne.cs.ualberta.ca:1521:CRS";
 		String username;
 		String password;
-		Connection conn = null;*/
+		Connection conn = null;
 
 		System.out.println("CMPUT 291 Project 1");
-
-		if (argv.length != 2)
+		// Obtains database username and password
+		if (argv.length != 2){
 			System.out.println("Enter oracle username followed by password");
-
-		/*try {
+			System.exit(0);
+		}
+		username = argv[0];
+		password = argv[1];
+		
+		// Attempts to connect to Oracle database
+		try {
 			Class.forName(driver);
 		} catch (ClassNotFoundException e) {
 			System.out.println("Oracle JDBC Driver not found");
 			return;
 		}
-
 		try{
-			conn = DriverManager.getConnected(database, username, password);
+			conn = DriverManager.getConnection(database, username, password);
 		} catch (SQLException e) {
 			System.out.println("Connection to Oracle failed");
 			return;
- 
 		}
-		
 		if (conn != null)
-			System.out.println("Connection established");*/
+			System.out.println("Connection established");
 	}
 }
