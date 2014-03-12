5b7be19481056c8e98675898c3c913bb3377746f
gaboriau <gaboriau@ualberta.ca>
2014-03-11 21:03:50 -0600

    Violation record fully implemented

--- Violation.java
+++ Violation.java
@@ -11,9 +11,12 @@ public class Violation {
 	
 	public static void violationRecord(Connection dbConn) {
 		PreparedStatement checkTicketCount, checkOffender, checkSerial;
+		PreparedStatement addTicket, checkOfficer, checkType;
 		ResultSet ticketCount, offenderCount, serialCount;
+		ResultSet officerCount, typeCount;
 		int ticketID = 0;
-		String offenderID, serialNum, inputDate;
+		String offenderID, serialNum, inputDate, place, comments;
+		String officerID, ticketType;
 		Scanner keyboard;
 		java.sql.Date violationDate;
 		java.util.Date vDate = new java.util.Date();
@@ -25,6 +28,12 @@ public class Violation {
 		String queryOffenderCount = "SELECT COUNT(sin) FROM people WHERE sin = ?";
 
 		String querySerialCount = "SELECT COUNT(serial_no) FROM vehicle WHERE serial_no = ?";
+
+		String queryOfficerCount = "SELECT COUNT(sin) FROM people WHERE sin = ? AND name LIKE %Officer%";
+
+		String queryTicketType = "SELECT COUNT(vtype) FROM ticket_type WHERE vtype = ?";
+
+		String queryAddTicket = "INSERT INTO ticket VALUES(?, ?, ?, ?, ?, ?, ?, ?)";
 
 		System.out.println("You have selected violation record");
 
@@ -87,9 +96,54 @@ public class Violation {
 			}
 		}
 
-		// TODO Obtain officer id
+		// Obtain officer id
+		while(true) {
+			System.out.print("Please enter the officer's SIN: ");
+			keyboard = new Scanner(System.in);
+			officerID = keyboard.nextLine();
+			if (offenderID.length() > 15)
+				System.out.println("SIN invalid");
+			else {
+				try {
+					checkOfficer = dbConn.prepareStatement(queryOfficerCount);
+					checkOfficer.setString(1, officerID);
+					officerCount = checkOfficer.executeQuery();
+					officerCount.next();
+					if (officerCount.getInt(1) == 0) {
+						officerCount.close();
+						System.out.println("No matching officer on record");
+					} else 
+						break;
+				} catch (SQLException e) {
+					System.out.println(e.getMessage());
+				}
+			}
+		}
 
-		// TODO Obtain violation type
+		// Obtain violation type
+		while(true) {
+			System.out.print("Please enter ticket type: ");
+
+			keyboard = new Scanner(System.in);
+			ticketType = keyboard.nextLine();
+			if (serialNum.length() > 10)
+				System.out.println("Invalid type");
+			else {
+				try {
+					checkType = dbConn.prepareStatement(queryTicketType);
+					checkType.setString(1, ticketType);
+					typeCount = checkType.executeQuery();
+					typeCount.next();
+					if (typeCount.getInt(1) == 0) {
+						typeCount.close();	
+						System.out.println("Type not in database");
+					} else 
+						break;
+				} catch (SQLException e) {
+					System.out.println(e.getMessage());
+				}
+			}
+		}
 
 		// Obtain violation date
 		while(true) {
@@ -110,11 +164,45 @@ public class Violation {
 		}
 		violationDate = new java.sql.Date(vDate.getTime());
 
-		// TODO Obtain violation place
+		// Obtain violation place
+		while(true) {
+			System.out.print("Please enter the location where the violation took place: ");
+			keyboard = new Scanner(System.in);
+			place = keyboard.nextLine();
+			if (place.length() > 20)
+				System.out.println("Place can be no more than 20 characters");
+			else
+				break;
+		}
 
-		// TODO Obtain descriptions
+		// Obtain descriptions
+		while(true) {
+			System.out.print("Please enter any further comments: ");
+			keyboard = new Scanner(System.in);
+			comments = keyboard.nextLine();
+			if (place.length() > 1024)
+				System.out.println("Comments can be no more than 1024 characters");
+			else
+				break;
+		}
 
-		// TODO Add violation record to database
+		// Add violation record to database
+		try {
+			addTicket = dbConn.prepareStatement(queryAddTicket);
+			addTicket.setInt(1, ticketID);
+			addTicket.setString(2, offenderID);
+			addTicket.setString(3, serialNum);
+			addTicket.setString(4, officerID);
+			addTicket.setString(5, ticketType);
+			addTicket.setDate(6, violationDate);
+			addTicket.setString(7, place);
+			addTicket.setString(8, comments);
+			addTicket.executeUpdate();
+			System.out.println("Ticket has been added to database");
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+
 	}
 
 }
